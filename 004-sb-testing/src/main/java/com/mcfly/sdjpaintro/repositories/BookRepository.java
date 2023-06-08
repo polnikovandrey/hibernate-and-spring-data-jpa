4906f196148ffd92baeb303a7dd92b1a0cd3b5b9
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-08 18:49:30 +0300

    - modules per episode

--- 004-sb-testing/src/main/java/com/mcfly/sdjpaintro/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mcfly.sdjpaintro.repositories;
-
-import com.mcfly.sdjpaintro.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
