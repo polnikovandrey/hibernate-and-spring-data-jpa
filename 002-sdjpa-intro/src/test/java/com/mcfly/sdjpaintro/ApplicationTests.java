7ea60f7277d5c3ca1a51bf74f1f61d12eea68c7c
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-08 18:58:45 +0300

    + ApplicationTests.testBookRepository

--- 002-sdjpa-intro/src/test/java/com/mcfly/sdjpaintro/ApplicationTests.java
+++ 002-sdjpa-intro/src/test/java/com/mcfly/sdjpaintro/ApplicationTests.java
@@ -1,10 +1,23 @@
 package com.mcfly.sdjpaintro;
 
+import com.mcfly.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository() {
+		final long count = bookRepository.count();
+		assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
