58d0d6611fb152821c2b67c43b6615da742a7b2f
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-10 13:11:23 +0300

    + AuthorDao.getById implementation

--- 009-sdjpa-jdbc/src/main/java/com/mcfly/sdjpajdbc/dao/AuthorDaoImpl.java
+++ 009-sdjpa-jdbc/src/main/java/com/mcfly/sdjpajdbc/dao/AuthorDaoImpl.java
@@ -3,11 +3,36 @@ package com.mcfly.sdjpajdbc.dao;
 import com.mcfly.sdjpajdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
+    private final DataSource dataSource;
+
+    public AuthorDaoImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public Author getById(Long id) {
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM author where id = " + id)) {
+            if (resultSet.next()) {
+                final Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
