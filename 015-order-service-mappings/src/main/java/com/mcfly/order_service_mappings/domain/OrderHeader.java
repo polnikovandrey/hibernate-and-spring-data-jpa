ad4fce0a73728edb29bae3e779dedbb92406e6a1
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-14 16:08:54 +0300

    + @Embeddable class (Address)

--- 015-order-service-mappings/src/main/java/com/mcfly/order_service_mappings/domain/OrderHeader.java
+++ 015-order-service-mappings/src/main/java/com/mcfly/order_service_mappings/domain/OrderHeader.java
@@ -1,13 +1,52 @@
 package com.mcfly.order_service_mappings.domain;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 import java.util.Objects;
 
 @Entity
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "shippingAddress.address",
+                column = @Column(name = "shipping_address")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.city",
+                column = @Column(name = "shipping_city")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.state",
+                column = @Column(name = "shipping_state")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.zipCode",
+                column = @Column(name = "shipping_zip_code")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.address",
+                column = @Column(name = "bill_to_address")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.city",
+                column = @Column(name = "bill_to_city")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.state",
+                column = @Column(name = "bill_to_state")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.zipCode",
+                column = @Column(name = "bill_to_zip_code")
+        )
+})
 public class OrderHeader extends BaseEntity {
 
     private String customerName;
+    private Address shippingAddress;
+    private Address billToAddress;
 
     public OrderHeader() {
     }
@@ -21,16 +60,19 @@ public class OrderHeader extends BaseEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-
         OrderHeader that = (OrderHeader) o;
-
-        return Objects.equals(customerName, that.customerName);
+        if (!Objects.equals(customerName, that.customerName)) return false;
+        if (!Objects.equals(shippingAddress, that.shippingAddress))
+            return false;
+        return Objects.equals(billToAddress, that.billToAddress);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + (customerName != null ? customerName.hashCode() : 0);
+        result = 31 * result + (shippingAddress != null ? shippingAddress.hashCode() : 0);
+        result = 31 * result + (billToAddress != null ? billToAddress.hashCode() : 0);
         return result;
     }
 
