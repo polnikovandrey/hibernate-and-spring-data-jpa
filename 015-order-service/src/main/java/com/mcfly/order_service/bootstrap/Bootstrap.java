9dacdf39698d6b14033f108825f9a0bb784d482b
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-15 17:20:32 +0300

    + Bootstrap#performOptimisticLockingDemo (@Version optimistic locking)

--- 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
+++ 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
@@ -22,11 +22,29 @@ public class Bootstrap implements CommandLineRunner {
     @Override
     public void run(String... args) {
         bootstrapOrderService.readOrderData();
+        performOptimisticLockingDemo();
+    }
 
+    private void performOptimisticLockingDemo() {
         final Customer customer = new Customer();
+
         customer.setCustomerName("Testing version");
         final Customer savedCustomer = customerRepository.save(customer);
         System.out.println("### version is: " + savedCustomer.getVersion());
-        customerRepository.deleteById(savedCustomer.getId());
+
+        savedCustomer.setCustomerName("Testing version 2");
+        final Customer savedCustomer2 = customerRepository.save(savedCustomer);
+        System.out.println("### version is: " + savedCustomer2.getVersion());
+
+        savedCustomer2.setCustomerName("Testing version 3");
+        /*
+            @Version Customer#version is checked before a save takes place.
+            customerRepository.save(savedCustomer); will throw an exception - the actual version was changed already.
+            @Version locking mechanism is being called "optimistic locking".
+         */
+        final Customer savedCustomer3 = customerRepository.save(savedCustomer2);
+        System.out.println("### version is: " + savedCustomer3.getVersion());
+
+        customerRepository.deleteById(savedCustomer3.getId());
     }
 }
