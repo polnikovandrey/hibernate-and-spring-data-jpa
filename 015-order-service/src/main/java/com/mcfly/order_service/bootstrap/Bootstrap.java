ce55ac5a10cd31174cbca1d62f3c2c9c3de26846
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-15 16:04:45 +0300

    + class Bootstrap implements CommandLineRunner

--- 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
+++ 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         System.out.println("I was called!");
     }
 }
