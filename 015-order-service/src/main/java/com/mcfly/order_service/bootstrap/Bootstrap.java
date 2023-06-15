6a40af37c7014a1a22b14c2344763f587964f8bc
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-15 16:53:50 +0300

    + BootstrapOrderService to make use of @Transactional annotation (not working within [Bootstrap extends CommandLineRunner])

--- 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
+++ 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
@@ -1,39 +1,19 @@
 package com.mcfly.order_service.bootstrap;
 
-import com.mcfly.order_service.domain.OrderHeader;
-import com.mcfly.order_service.repository.OrderHeaderRepository;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.Logger;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-    public static final Logger LOGGER = Logger.getLogger(Bootstrap.class.getSimpleName());
+    private final BootstrapOrderService bootstrapOrderService;
 
-    private final OrderHeaderRepository orderHeaderRepository;
-
-    public Bootstrap(OrderHeaderRepository orderHeaderRepository) {
-        this.orderHeaderRepository = orderHeaderRepository;
+    public Bootstrap(BootstrapOrderService bootstrapOrderService) {
+        this.bootstrapOrderService = bootstrapOrderService;
     }
 
-    @Transactional
     @Override
     public void run(String... args) {
-        final OrderHeader orderHeader = orderHeaderRepository.findById(1L).orElseThrow(EntityNotFoundException::new);
-        orderHeader.getOrderLines()
-                   .forEach(orderLine -> {
-                       LOGGER.info(orderLine.getProduct().getDescription());
-                       /*
-                            orderLine.getProduct().getCategories() will fail without @Transactional on method:
-                             field categories is LAZY, could be fetched within a hibernate session (transaction) only.
-                        */
-                       orderLine.getProduct()
-                                .getCategories()
-                                .forEach(category -> LOGGER.info(category.getDescription()));
-                   });
+        bootstrapOrderService.readOrderData();
     }
 }
---
+++ 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/BootstrapOrderService.java
@@ -0,0 +1,37 @@
+package com.mcfly.order_service.bootstrap;
+
+import com.mcfly.order_service.domain.OrderHeader;
+import com.mcfly.order_service.repository.OrderHeaderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BootstrapOrderService {
+
+    private final OrderHeaderRepository orderHeaderRepository;
+
+    public BootstrapOrderService(OrderHeaderRepository orderHeaderRepository) {
+        this.orderHeaderRepository = orderHeaderRepository;
+    }
+
+    @Transactional
+    public void readOrderData() {
+        final OrderHeader orderHeader = orderHeaderRepository.findById(1L).orElseThrow(EntityNotFoundException::new);
+        orderHeader.getOrderLines()
+                   .forEach(orderLine -> {
+                       printInfo(orderLine.getProduct().getDescription());
+                       /*
+                            orderLine.getProduct().getCategories() will fail without @Transactional on method:
+                             field categories is LAZY, could be fetched within a hibernate session (transaction) only.
+                        */
+                       orderLine.getProduct()
+                                .getCategories()
+                                .forEach(category -> printInfo(category.getDescription()));
+                   });
+    }
+
+    private void printInfo(String text) {
+        System.out.println("### " + text);
+    }
+}
