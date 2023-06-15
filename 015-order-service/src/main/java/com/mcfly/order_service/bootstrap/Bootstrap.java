c0dc89a493c846e3749a39cac46977ec99cb84c6
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-15 17:05:30 +0300

    + @Version Customer#version

--- 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
+++ 015-order-service/src/main/java/com/mcfly/order_service/bootstrap/Bootstrap.java
@@ -1,5 +1,8 @@
 package com.mcfly.order_service.bootstrap;
 
+import com.mcfly.order_service.domain.Customer;
+import com.mcfly.order_service.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -7,13 +10,23 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
 
     private final BootstrapOrderService bootstrapOrderService;
+    private final CustomerRepository customerRepository;
 
-    public Bootstrap(BootstrapOrderService bootstrapOrderService) {
+    @Autowired
+    public Bootstrap(BootstrapOrderService bootstrapOrderService,
+                     CustomerRepository customerRepository) {
         this.bootstrapOrderService = bootstrapOrderService;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) {
         bootstrapOrderService.readOrderData();
+
+        final Customer customer = new Customer();
+        customer.setCustomerName("Testing version");
+        final Customer savedCustomer = customerRepository.save(customer);
+        System.out.println("### version is: " + savedCustomer.getVersion());
+        customerRepository.deleteById(savedCustomer.getId());
     }
 }
--- 015-order-service/src/main/java/com/mcfly/order_service/domain/Customer.java
+++ 015-order-service/src/main/java/com/mcfly/order_service/domain/Customer.java
@@ -3,9 +3,9 @@ package com.mcfly.order_service.domain;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Version;
 
 import java.util.LinkedHashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -18,6 +18,8 @@ public class Customer extends BaseEntity {
     private String email;
     @OneToMany(mappedBy = "customer")
     private Set<OrderHeader> orders;
+    @Version
+    private Integer version;
 
     public Customer() {
         this.orders = new LinkedHashSet<>();
@@ -28,30 +30,6 @@ public class Customer extends BaseEntity {
         this.address = address;
         this.phone = phone;
         this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Customer customer = (Customer) o;
-        if (!Objects.equals(customerName, customer.customerName))
-            return false;
-        if (!Objects.equals(address, customer.address)) return false;
-        if (!Objects.equals(phone, customer.phone)) return false;
-        if (!Objects.equals(email, customer.email)) return false;
-        return Objects.equals(orders, customer.orders);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (customerName != null ? customerName.hashCode() : 0);
-        result = 31 * result + (address != null ? address.hashCode() : 0);
-        result = 31 * result + (phone != null ? phone.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        return result;
     }
 
     public String getCustomerName() {
@@ -92,5 +70,13 @@ public class Customer extends BaseEntity {
 
     public void setOrders(Set<OrderHeader> orders) {
         this.orders = orders;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 }
