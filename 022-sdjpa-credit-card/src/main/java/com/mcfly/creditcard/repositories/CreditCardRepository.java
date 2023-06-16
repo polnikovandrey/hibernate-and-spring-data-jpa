9c81da63bb951412f5a16e9aae7e5902297ebbc5
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-16 16:18:05 +0300

    + sdjpa-credit-card module

---
+++ 022-sdjpa-credit-card/src/main/java/com/mcfly/creditcard/repositories/CreditCardRepository.java
@@ -0,0 +1,10 @@
+package com.mcfly.creditcard.repositories;
+
+import com.mcfly.creditcard.domain.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by jt on 6/27/22.
+ */
+public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
+}
