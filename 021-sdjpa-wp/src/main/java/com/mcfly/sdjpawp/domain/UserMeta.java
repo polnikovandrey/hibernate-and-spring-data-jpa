4110dbf1c9c0baaf45e115d9a1679d9b1c944315
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-16 13:43:12 +0300

    + @Entity UserMeta + @Lob mapping for a db longtext column

---
+++ 021-sdjpa-wp/src/main/java/com/mcfly/sdjpawp/domain/UserMeta.java
@@ -0,0 +1,50 @@
+package com.mcfly.sdjpawp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+    private Long id;
+    private Long userId;
+    private String metaKey;
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String metaValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMetaKey() {
+        return metaKey;
+    }
+
+    public void setMetaKey(String metaKey) {
+        this.metaKey = metaKey;
+    }
+
+    public String getMetaValue() {
+        return metaValue;
+    }
+
+    public void setMetaValue(String metaValue) {
+        this.metaValue = metaValue;
+    }
+}
