e43855cc7d7c875ffcda11d8006dd684e5d7e4bd
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-16 17:12:01 +0300

    + EncryptionInterceptor final implementation

---
+++ 022-sdjpa-credit-card/src/main/java/com/mcfly/creditcard/interceptors/EncryptedString.java
@@ -0,0 +1,9 @@
+package com.mcfly.creditcard.interceptors;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface EncryptedString {
+}
--- 022-sdjpa-credit-card/src/main/java/com/mcfly/creditcard/interceptors/EncryptionInterceptor.java
+++ 022-sdjpa-credit-card/src/main/java/com/mcfly/creditcard/interceptors/EncryptionInterceptor.java
@@ -1,28 +1,72 @@
 package com.mcfly.creditcard.interceptors;
 
+import com.mcfly.creditcard.services.EncryptionService;
 import org.hibernate.CallbackException;
 import org.hibernate.Interceptor;
 import org.hibernate.type.Type;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class EncryptionInterceptor implements Interceptor {
 
+    private final EncryptionService encryptionService;
+
+    public EncryptionInterceptor(EncryptionService encryptionService) {
+        this.encryptionService = encryptionService;
+    }
+
     @Override
     public boolean onSave(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
         System.out.println("$$$ onSave()");
-        return Interceptor.super.onSave(entity, id, state, propertyNames, types);
+        final Object[] newState = processFields(entity, state, propertyNames, "onSave");
+        return Interceptor.super.onSave(entity, id, newState, propertyNames, types);
     }
 
     @Override
     public boolean onLoad(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
         System.out.println("$$$ onLoad()");
-        return Interceptor.super.onLoad(entity, id, state, propertyNames, types);
+        final Object[] newState = processFields(entity, state, propertyNames, "onLoad");
+        return Interceptor.super.onLoad(entity, id, newState, propertyNames, types);
     }
 
     @Override
     public boolean onFlushDirty(Object entity, Object id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) throws CallbackException {
         System.out.println("$$$ onFlushDirty()");
-        return Interceptor.super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+        final Object[] newState = processFields(entity, currentState, propertyNames, "onFlushDirty");
+        return Interceptor.super.onFlushDirty(entity, id, newState, previousState, propertyNames, types);
+    }
+
+    private Object[] processFields(Object entity, Object[] state, String[] propertyNames, String type) {
+        final List<String> annotationFields = getAnnotationFields(entity);
+        for (String field: annotationFields) {
+            for (int i = 0; i < propertyNames.length; i++) {
+                if (propertyNames[i].equals(field)) {
+                    if (StringUtils.hasText(state[i].toString())) {
+                        if ("onSave".equals(type) || "onFlushDirty".equals(type)) {
+                             state[i] = encryptionService.encrypt(state[i].toString());
+                        } else if ("onLoad".equals(type)) {
+                             state[i] = encryptionService.decrypt(state[i].toString());
+                        }
+                    }
+                }
+            }
+        }
+        return state;
+    }
+
+    private List<String> getAnnotationFields(Object entity) {
+        final List<String> annotationFields = new ArrayList<>();
+        for (Field field : entity.getClass().getDeclaredFields()) {
+            if (!Objects.isNull(field.getAnnotation(EncryptedString.class))) {
+                annotationFields.add(field.getName());
+            }
+        }
+        return annotationFields;
     }
 }
