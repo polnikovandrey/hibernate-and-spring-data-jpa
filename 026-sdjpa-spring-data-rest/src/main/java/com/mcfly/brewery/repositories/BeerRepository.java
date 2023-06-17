9384bce6c25425fdbd751b4e4fe497372baee3af
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-17 15:57:19 +0300

    + corrected api path beers -> beer

--- 026-sdjpa-spring-data-rest/src/main/java/com/mcfly/brewery/repositories/BeerRepository.java
+++ 026-sdjpa-spring-data-rest/src/main/java/com/mcfly/brewery/repositories/BeerRepository.java
@@ -6,11 +6,13 @@ import com.mcfly.brewery.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
-
+@RepositoryRestResource(path = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
     Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
