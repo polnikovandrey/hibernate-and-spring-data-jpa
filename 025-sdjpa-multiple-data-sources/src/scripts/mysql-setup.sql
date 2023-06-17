4720d8ae7102ec612dbce273b3ab9cf08eb56213
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-17 09:14:59 +0300

    + mysql-setup.sql (card db & cardholder db)

--- 025-sdjpa-multiple-data-sources/src/scripts/mysql-setup.sql
+++ 025-sdjpa-multiple-data-sources/src/scripts/mysql-setup.sql
@@ -19,3 +19,14 @@ grant select, insert, update, delete, create, drop, references, insert, alter, e
 create user if not exists `carduser`@`%` identified with mysql_native_password by 'password';
 grant select, insert, update, delete on `card`.* to `carduser`@`%`;
 flush privileges;
+
+-- card PAN db
+drop database if exists pan;
+drop user if exists `panadmin`@`%`;
+drop user if exists `panuser`@`%`;
+create database if not exists pan character set utf8mb3 collate utf8mb4_unicode_ci;
+create user if not exists `panadmin`@`%` identified with mysql_native_password by 'password';
+grant select, insert, update, delete, create, drop, references, insert, alter, event, create view, show view, create routine, alter routine, event, trigger on `pan`.* to `panadmin`@`%`;
+create user if not exists `panuser`@`%` identified with mysql_native_password by 'password';
+grant select, insert, update, delete on `pan`.* to `panuser`@`%`;
+flush privileges;
